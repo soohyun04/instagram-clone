0aea1a30f9a06456a8dee72b90a5aea5b34bdfa2
soohyun04 <yusuhyun04@gmail.com>
2026-03-24 12:08:06 +0900

    jwt cookie 세팅

--- src/main/java/com/project/instagramclone/common/CookieUtil.java
+++ src/main/java/com/project/instagramclone/common/CookieUtil.java
@@ -1,16 +1,18 @@
 package com.project.instagramclone.common;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
 import java.util.Arrays;
+// service controller 처럼 이름을 규정지어 만든 것이 아니라 개발자가 필요로 해서 만든 자바 클래스 파일
 
 @Slf4j
-@Component //service controller 처럼 이름을 규정지어 만든 것이 아니라 개발자가 필요로 해서 만든 자바 클래스 파일
+@Component
 public class CookieUtil {
-    public void add(HttpServletResponse res, String name, String value, int maxAgeSeconds){
+
+    public void add(HttpServletResponse res, String name, String value, int maxAgeSeconds) {
         Cookie cookie = new Cookie(name, value);
         cookie.setHttpOnly(true);
         cookie.setPath("/");
@@ -18,17 +20,17 @@ public class CookieUtil {
         res.addCookie(cookie);
     }
 
-    public void delete(HttpServletResponse res, String name){
+
+    public void delete(HttpServletResponse res, String name) {
         Cookie cookie = new Cookie(name, "");
-        cookie.setPath("/");
         cookie.setMaxAge(0);
+        cookie.setPath("/");
         res.addCookie(cookie);
     }
 
-    public String get(HttpServletResponse req, String name){
+    public String get(HttpServletRequest req, String name) {
         Cookie[] cookies = req.getCookies();
-
-        if (cookies == null) return null;
+        if(cookies == null) return  null;
 
         return Arrays.stream(cookies)
                 .filter(c -> name.equals(c.getName()))
